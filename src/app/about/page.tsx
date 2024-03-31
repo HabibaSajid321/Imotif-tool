4d4d2f24ee025c78453725a74f5e168eb4555b33
HabibaSajid321 <habibachudhary434@gmail.com>
2024-03-31 15:20:46 +0500

    about responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,9 +24,9 @@ const About = () => {
             </span>
           </h1>
         </div>
-        <div className="flex flex-col justify-center content-center items-center">
-          <div className="flex md:px-32 px-10  p-9 flex-row border-2 border-gray-300 m-10 gap-10 rounded-2xl shadow-2xl lg:w-[1180px] w-full ">
-            <div className="w-1/3">
+        <div className="flex flex-col justify-center content-center items-center w-full md:p-0 p-2">
+          <div className="  flex flex-col md:flex-row md:px-32 px-10  p-9  border-2 border-gray-300 m-10 gap-10 rounded-2xl shadow-2xl lg:w-[1180px] w-full  ">
+            <div className="w-full md:w-1/3">
               <Image
                 className="md:w-[280px] w-[140px] md:h-[270px] h-[120px] md:rounded-full rounded-2xl shadow-3xl"
                 src={VCimage}
@@ -35,8 +35,8 @@ const About = () => {
               />
             </div>
 
-            <div className="w-2/3 pt-8">
-              <div className="flex flex-col justify-center content-center   ">
+            <div className="w-full md:w-2/3 md:pt-8 pt-0">
+              <div className="flex flex-col justify-center content-center">
                 <h1 className="text-[#1f7f6d] md:font-semibold font-medium md:text-xl text-sm md:px-[20px] px-4 text-left ">
                   Prof. Dr. Rana Iqrar Ahmed Khan
                   <span className="inline text-[#6e706f] pl-2  text-base">
@@ -46,7 +46,7 @@ const About = () => {
                 <h3 className="text-[#565757]  md:text-base text-xs md:px-[20px] px-4 text-left ">
                   Vice Chancellor
                 </h3>
-                <p className="text-black pt-4 md:text-sm text-xs md:pl-5 hidden md:block  text-justify">
+                <p className="text-black pt-4 md:text-sm text-xs md:pl-5 text-justify">
                   Under the leadership of the Vice Chancellor, the University of
                   Agriculture Faisalabad is pioneering research in the field of
                   BioInformatics. Directed by their vision, the university has
@@ -59,20 +59,8 @@ const About = () => {
             </div>
           </div>
 
-          <div>
-            {" "}
-            <p className="text-black pt-4  w-full text-sm px-12 md:hidden block ">
-              Under the leadership of the Vice Chancellor, the University of
-              Agriculture Faisalabad is pioneering research in the field of
-              BioInformatics. Directed by their vision, the university has
-              dedicated resources towards the creation of cutting-edge data
-              tools aimed at investigating the thermal stability of i-motif
-              structures, leveraging high-performance systems for advanced
-              analysis.
-            </p>
-          </div>
-          <div className="flex md:px-32 px-10  p-9 flex-row border-2 border-gray-300 m-10 gap-10 rounded-2xl shadow-2xl lg:w-[1180px] w-full  ">
-            <div className="w-1/3">
+          <div className=" flex flex-col md:flex-row md:px-32 px-10  p-9  border-2 border-gray-300 m-10 gap-10 rounded-2xl shadow-2xl lg:w-[1180px] w-full   ">
+            <div className="md:w-1/3 w-full">
               <Image
                 className="md:w-[280px] w-[140px] md:h-[270px] h-[120px] md:rounded-full rounded-2xl shadow-3xl"
                 src={EfsDImage}
@@ -80,7 +68,7 @@ const About = () => {
                 loading="eager"
               />
             </div>
-            <div className="w-2/3 md:pt-8 pt-6">
+            <div className="md:w-2/3 w-full md:pt-8 pt-0">
               <div className="flex flex-col justify-center content-center   ">
                 <h1 className="text-[#1f7f6d] md:font-semibold font-medium md:text-xl text-sm md:px-[20px] px-4 text-left  ">
                   Prof. Dr. Ijaz Ahmad Bhatti
@@ -88,7 +76,7 @@ const About = () => {
                 <h3 className="text-[#565757]  md:text-base text-xs md:px-[20px] px-4 text-left">
                   Executive Director EFS
                 </h3>
-                <p className="text-black pt-4 md:text-sm text-xs md:pl-5 hidden md:block text-justify ">
+                <p className="text-black pt-4 md:text-sm text-xs md:pl-5 text-justify">
                   The Director of the Endowment Fund Secretariat and Dean
                   Faculty of Sciences at the University of Agriculture,
                   Faisalabad UAF plays a pivotal role in ensuring the effective
@@ -99,17 +87,9 @@ const About = () => {
               </div>
             </div>
           </div>
-          <div>
-            <p className="text-black pt-4  w-full text-sm px-12 md:hidden block ">
-              The Director of the Endowment Fund Secretariat and Dean Faculty of
-              Sciences at the University of Agriculture, Faisalabad UAF plays a
-              pivotal role in ensuring the effective utilization of resources
-              towards advancing {"Pakistan's"} long-term research and
-              development goals in the agriculture and health sector.
-            </p>
-          </div>
-          <div className="flex md:px-32 px-10  p-9 flex-row border-2 border-gray-300 m-10 gap-10 rounded-2xl shadow-2xl lg:w-[1180px] w-full ">
-            <div className="w-1/3">
+
+          <div className="flex flex-col md:flex-row md:px-32 px-10  p-9  border-2 border-gray-300 m-10 gap-10 rounded-2xl shadow-2xl lg:w-[1180px] w-full  ">
+            <div className="md:w-1/3 w-full">
               <Image
                 className="md:w-[280px] w-[140px] md:h-[270px] h-[120px] md:rounded-full rounded-2xl shadow-3xl"
                 src={CasDirector}
@@ -117,7 +97,7 @@ const About = () => {
                 loading="eager"
               />
             </div>
-            <div className="w-2/3 md:pt-8 pt-6">
+            <div className="md:w-2/3 w-full md:pt-8 pt-0">
               <div className="flex flex-col justify-center content-center   ">
                 <h1 className="text-[#1f7f6d] md:font-semibold font-medium md:text-xl text-sm md:px-[20px] px-4 text-left  ">
                   Prof. Dr. Sultan Habibullah Khan
@@ -125,7 +105,7 @@ const About = () => {
                 <h3 className="text-[#565757]  md:text-base text-xs md:px-[20px] px-4 text-left">
                   Director CAS-AFS
                 </h3>
-                <p className="text-black pt-4 md:text-sm text-xs md:pl-5 hidden md:block text-justify  ">
+                <p className="text-black pt-4 md:text-sm text-xs md:pl-5  text-justify  ">
                   The Director CAS-AFS at the University of Agriculture,
                   Faisalabad UAF is a visionary leader dedicated to driving
                   excellence in agricultural education, research, and outreach.
@@ -138,19 +118,7 @@ const About = () => {
               </div>
             </div>
           </div>
-          <div>
-            {" "}
-            <p className="text-black pt-4  w-full text-sm px-12 md:hidden block ">
-              The Director CAS-AFS at the University of Agriculture, Faisalabad
-              UAF is a visionary leader dedicated to driving excellence in
-              agricultural education, research, and outreach. With a deep
-              commitment to addressing national and global challenges in food
-              security and sustainable agriculture, the director spearheads
-              efforts to bridge the gap between academia, industry, and
-              government. He is ensuring all the provision of all facilities for
-              this project.
-            </p>
-          </div>
+
           <div className=" flex flex-col content-center justify-center text-black md:text-4xl text-lg font-semibold md:pt-16 pt-10">
             <h1 className="text-center">Our Project Team</h1>
             <div className="md:pt-4 pt-1 flex content-center justify-center">
@@ -158,8 +126,8 @@ const About = () => {
             </div>
           </div>
 
-          <div className="flex md:px-32 px-10  p-9 flex-row border-2 border-gray-300 m-10 gap-10 rounded-2xl shadow-2xl lg:w-[1180px] w-full">
-            <div className="w-1/3 pt-8">
+          <div className="flex flex-col md:flex-row md:px-32 px-10  p-9  border-2 border-gray-300 m-10 gap-10 rounded-2xl shadow-2xl lg:w-[1180px] w-full ">
+            <div className="md:w-1/3 w-full pt-8">
               <Image
                 className="md:w-[280px] w-[140px] md:h-[270px] h-[120px] md:rounded-full rounded-2xl shadow-3xl "
                 src={Mamimg}
@@ -167,15 +135,15 @@ const About = () => {
                 loading="eager"
               />
             </div>
-            <div className="w-2/3 ">
-              <div className="flex flex-col justify-center content-center pt-10  ">
+            <div className="md:w-2/3 w-full ">
+              <div className="flex flex-col justify-center content-center md:pt-10 pt-0  ">
                 <h1 className="text-[#1f7f6d] md:font-semibold font-medium md:text-xl text-sm md:px-[20px] px-4 text-left  ">
                   Dr. Amen Shamim
                 </h1>
                 <h3 className="text-[#565757]  md:text-base text-xs md:px-[20px] px-4 text-left ">
                   Assistant Professor
                 </h3>
-                <p className="text-black pt-4 md:text-base text-xs md:pl-5 hidden md:block  text-justify ">
+                <p className="text-black pt-4 md:text-base text-xs md:pl-5  text-justify ">
                   Dr. Amen Shamim, Principal Investigator of this project,
                   specialized as a bioinformatician with a profound interest in
                   data analysis. Her research centers on leveraging
@@ -192,19 +160,8 @@ const About = () => {
             </div>
           </div>
 
-          <div>
-            {" "}
-            <p className="text-black pt-4  w-full text-sm px-12 md:hidden block ">
-              Dr. Amen Shamim, Principal Investigator of this project,
-              specialized as a bioinformatician with a profound interest in data
-              analysis. Her research centers on leveraging computational tools
-              and big data analysis to tackle intricate health issues at the
-              convergence of human, animal, and environmental health.
-            </p>
-          </div>
-
-          <div className="flex md:px-32 px-10  p-9 flex-row border-2 border-gray-300 m-10 gap-10 rounded-2xl shadow-2xl lg:w-[1180px] w-full ">
-            <div className="w-1/3">
+          <div className="flex flex-col md:flex-row md:px-32 px-10  p-9  border-2 border-gray-300 m-10 gap-10 rounded-2xl shadow-2xl lg:w-[1180px] w-full ">
+            <div className="md:w-1/3 w-full">
               <Image
                 className="md:w-[280px] w-[140px] md:h-[270px] h-[120px] md:rounded-full rounded-2xl shadow-3xl"
                 src={HSimg}
@@ -212,7 +169,7 @@ const About = () => {
                 loading="eager"
               />
             </div>
-            <div className="w-2/3 pt-12">
+            <div className="md:w-2/3 w-full md:pt-8 pt-0">
               <div className="flex flex-col justify-center content-center   ">
                 <h1 className="text-[#1f7f6d] md:font-semibold font-medium md:text-xl text-sm md:px-[20px] px-4 text-left  ">
                   Muhammad Hassan
@@ -220,7 +177,7 @@ const About = () => {
                 <h3 className="text-[#565757]  md:text-base text-xs md:px-[20px] px-4 text-left ">
                   Researcher
                 </h3>
-                <p className="text-black pt-4 md:text-sm text-xs md:pl-5 hidden md:block text-justify  ">
+                <p className="text-black pt-4 md:text-sm text-xs text-justify  ">
                   Mr. Muhammad Hassan Butt, a distinguished fellow researcher
                   within this project, possesses expertise in the fields of
                   structural bioinformatics and bioinformatics data analysis.
